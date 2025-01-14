6bb4e52bfbbb2eaf1d33d673608317fd1f2487f0
Kunal Handa <66132575+khanda11@users.noreply.github.com>
2025-01-14 17:38:54 +0000

    intial 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new Streamlit app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help KUNAL HANDAand inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
