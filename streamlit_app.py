2bafa57989a32b226fb8e11df1fb01d6369ac62a
Kunal Handa <66132575+khanda11@users.noreply.github.com>
2025-01-14 17:41:39 +0000

    kooomba

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlit app")
+st.title("🎈 My new KOOMBA Streamlit app")
 st.write(
     "Let's start building! For help KUNAL HANDAand inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
